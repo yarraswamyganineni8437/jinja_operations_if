816eb3db1ce796f6c95093a2ecbf986db225eb2e
yarraswamygangineni8437 <yadavgangineni@gmail.com>
2023-03-14 22:24:18 +0530

    first commit

--- operational/views.py
+++ operational/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 
 # Create your views here.
 def jinjaoperations(request):
-    d={'a':100,'b':20}
+    d={'a':100,'b':200}
     return render(request,'operational_htmlpage.html',context=d)
